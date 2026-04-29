49825b888eed0522d71a89dda4c9640bc3b6927f
Affa <asysyaffak@gmail.com>
2026-04-29 14:57:00 +0700

    Initial Commit

---
+++ ReSurface (Eileen ver.)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReSurface (Eileen ver.)
+//
+//  Created by Asy Syaffa Khoirunnisa on 29/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReSurface (Eileen ver.)/ReSurface__Eileen_ver__App.swift
@@ -0,0 +1,17 @@
+//
+//  ReSurface__Eileen_ver__App.swift
+//  ReSurface (Eileen ver.)
+//
+//  Created by Asy Syaffa Khoirunnisa on 29/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ReSurface__Eileen_ver__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
